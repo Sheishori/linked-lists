1681377999ef03645c0480f94e12ebd637925a41
Sheishori <sheishori@gmail.com>
2022-12-09 14:51:48 +0100

    Add a function adding new node at the end of the list

--- LinkedLists.js
+++ LinkedLists.js
@@ -7,5 +7,14 @@ const LinkedList = () => {
 		head = Node(value, head);
 	};
 
-	return { prepend };
+	const append = (value) => {
+		if (head === null) prepend(value);
+		else {
+			let tmp = head;
+			while (tmp.nextNode !== null) tmp = tmp.nextNode;
+			tmp.nextNode = Node(value);
+		}
+	};
+
+	return { prepend, append };
 };
