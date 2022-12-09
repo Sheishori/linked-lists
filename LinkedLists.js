3ba50effdaf6cdf55e819e10787e7e44c4557702
Sheishori <sheishori@gmail.com>
2022-12-09 16:20:00 +0100

    Add a function returning a node at the given index

--- LinkedLists.js
+++ LinkedLists.js
@@ -39,19 +39,31 @@ const LinkedList = () => {
 		return tmp;
 	};
 
+	const at = (index) => {
+		let tmp = _head;
+		let nodeIndex = 0;
+		while (nodeIndex !== index && tmp !== null) {
+			tmp = tmp.nextNode;
+			nodeIndex += 1;
+		}
+		if (tmp === null) return 'No node at given index';
+		return tmp;
+	};
+
 	const toString = (node = _head) => {
 		if (node === null) return 'null';
 		return `( ${node.value} ) -> ${toString(node.nextNode)}`;
 	};
 
-	return { prepend, append, size, head, tail, toString };
+	return { prepend, append, size, head, tail, at, toString };
 };
 
 const newList = LinkedList();
-newList.append('cheese');
-newList.append('bread');
-newList.prepend('bread');
-console.log('Size: ', newList.size());
-console.log('Head: ', newList.head());
-console.log('Tail: ', newList.tail());
-console.log(newList.toString());
+newList.append('1 - Butter');
+newList.append('2 - Cheese');
+newList.prepend('0 - Bread');
+//console.log('Size: ', newList.size());
+//console.log('Head: ', newList.head());
+//console.log('Tail: ', newList.tail());
+//console.log(newList.toString());
+console.log(newList.at(2));
