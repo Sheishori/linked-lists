bf0a4cd6d71c92ed3bfbc6d9146375b486d65fee
Sheishori <sheishori@gmail.com>
2022-12-09 15:29:58 +0100

    Add a function returning the total number of nodes in the list

--- LinkedLists.js
+++ LinkedLists.js
@@ -16,15 +16,27 @@ const LinkedList = () => {
 		}
 	};
 
+	const size = () => {
+		if (head === null) return 0;
+		let tmp = head;
+		let total = 1;
+		while (tmp.nextNode !== null) {
+			tmp = tmp.nextNode;
+			total += 1;
+		}
+		return total;
+	};
+
 	const toString = (node = head) => {
 		if (node === null) return 'null';
 		return `( ${node.value} ) -> ${toString(node.nextNode)}`;
 	};
 
-	return { prepend, append, toString };
+	return { prepend, append, size, toString };
 };
 
 const newList = LinkedList();
 newList.append('bread');
 newList.append('cheese');
+console.log(newList.size());
 console.log(newList.toString());
